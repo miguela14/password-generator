a5f568f4d074f0725f7f4ee8369ae120fe789d7a
unknown <miguelmoreno142@yahoo.com>
2023-03-23 12:27:36 -0400

    added forloop with correct code to generate a password return

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //Password can include special characters
 //Array of special characters
 
-//window.promt to generate password (Password length)
+// promt to generate password (Password length)
 //Password criteria- length of 8 to 128 characters, choice that includes lowercase, uppercase, numeric, and/or special characters 
 
 //promt "enter length of charaters"
@@ -14,17 +14,16 @@
 //store promts as var, at end run if statments 
 
 // Assignment Code
-let generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
  
 let specialChar = ["!", ".", "/", "$", "%", "^", "&", "*", "<", ">", "-", "+", ","];
 let numbers = [1,2,3,4,5,6,7,8,9,0];
-let alphabetLowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let alphabetUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 
 function generatePassword() {
-  
-  let userOptions = window.prompt("Enter a number for length of password from 8 to 128.");
+  let userOptions = prompt("Enter a number for length of password from 8 to 128.");
 
   let passwordChoice = parseInt(userOptions);
 
@@ -33,45 +32,47 @@ function generatePassword() {
   let wantsUppercase = false;
   let wantsSpecialCharacters = false;
 
+  if (isNaN(passwordChoice) || passwordChoice < 8 || passwordChoice > 128) {
+   alert("Invalid input! Password length must be a number from 8 to 128.");
+    return;
+  }
 
-  if (window.confirm("Do you want numbers in your password?")) {
-    wantsNumbers = true
+  if (confirm("Do you want numbers in your password?")) {
+    wantsNumbers = true;
   }
-  if (window.confirm("Do you want lowercase letters in your password?")) {
-    wantsLowercase = true
+  if (confirm("Do you want lowercase letters in your password?")) {
+    wantsLowercase = true;
   }
-  if (window.confirm("Do you want uppercase letters in your password?")) {
-    wantsUppercase = true
+  if  (confirm("Do you want uppercase letters in your password?")) {
+    wantsUppercase = true;
   }
-  if (window.confirm("Do you want special characters in your password?")) {
-    wantsSpecialCharacters = true
+  if  (confirm("Do you want special characters in your password?")) {
+    wantsSpecialCharacters = true;
   }
-  if (wantsNumbers) {
-    characters = characters.concat(numbers) 
-  }
+
   let characters = [];
+  if (wantsNumbers) {
+    characters = characters.concat(numbers);
+  }
   if (wantsLowercase) {
-    characters = characters.concat(alphabetLowerCase)
+    characters = characters.concat(alphabet);
   }
-
   if (wantsUppercase) {
-    characters = characters.concat(alphabetUpperCase)
+    characters = characters.concat(alphabetUpperCase);
   }
-
   if (wantsSpecialCharacters) {
-    characters = characters.concat(specialChar)
+    characters = characters.concat(specialChar);
   }
-  
+  if (characters.length === 0) {
+    alert("You must select at least one type of characters!");
+    return;
   }
 
-// [Math.floor(Math.random() * choices.length)];
-
-//take 8 to 128 and for loop through those numbers
-/*take math.random on charactes for the amount of time that the user put in
-  "if user wants ten numbers, generate 10 random index for the array"
-*/
-
-  return (characters);
+  let password = "";
+  for (let i = 0; i < passwordChoice; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return password;
 }
 
 // Write password to the #password input
